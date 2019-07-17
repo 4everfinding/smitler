0b7bb69ccdf04dc04bbf3861e7e1994f5035c8f5
4everfinding <4everfinding@gmail.com>
2019-07-17 23:49:51 +0500

    Made adaptive carousel of services list

--- app/js/common.js
+++ app/js/common.js
@@ -16,4 +16,57 @@ $(function() {
     $('.hamburger').removeClass('is-active')
   });
 
+  $('.services__carousel').on('initialized.owl.carousel', function() {
+    setTimeout(function() {
+      carouselServices()
+    }, 100);
+  });
+
+  $('.services__carousel').owlCarousel({
+    loop: true,
+    nav: true,
+    dots: false,
+    smartSpeed: 700,
+    navText: ['<i class="fas fa-angle-double-left"></i>', '<i class="fas fa-angle-double-right"></i>'],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      800: {
+        items: 2
+      },
+      1100: {
+        items: 3
+      },
+    }
+  });
+
+  $('.services__content').equalHeights();
+
+  function carouselServices() {
+    $('.services__item').each(function() {
+      var ths = $(this),
+          thsh = ths.find('.services__content').outerHeight();
+      ths.find('.services__image').css('min-height', thsh);
+    });
+  }
+
+  carouselServices();
+
+  $('.services__content-wrapper .services__title').each(function() {
+    var ths = $(this);
+    ths.html(ths.html().replace(/(\S+\s*$)/, '<span>$1</span>'));
+  });
+
+  // Resize window
+  function onResize() {
+    $('.services__content').equalHeights();
+  }
+
+  onResize();
+
+  window.onResize = function() {
+    onResize();
+  }
 });
