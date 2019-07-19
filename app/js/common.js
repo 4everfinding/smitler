a4fd8883e8f5ea137266c3e71215e97ee4545ebe
4everfinding <4everfinding@gmail.com>
2019-07-19 23:14:24 +0500

    Made a partner section

--- app/js/common.js
+++ app/js/common.js
@@ -90,6 +90,29 @@ $(function() {
     autoHeight: true
   });
 
+  $('.partners__carousel').owlCarousel({
+    loop: true,
+    items: 4,
+    smartSpeed: 700,
+    dots: false,
+    nav: true,
+    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      992: {
+        items: 3
+      },
+      1200: {
+        items: 4
+      }
+    }
+  });
   // Resize window
   function onResize() {
     $('.services__content').equalHeights();
